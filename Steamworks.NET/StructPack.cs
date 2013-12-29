bbecb48b912d9e6314daa01a13a23a88a54cc5d9
Riley Labrecque <rileylabrecque@gmail.com>
2013-12-29 05:29:11 -0600

    Redesigned Packsize

--- Steamworks.NET/StructPack.cs
+++ Steamworks.NET/StructPack.cs
@@ -1,34 +1,43 @@
+#if UNITY_STANDALONE_WIN
+#define VALVE_CALLBACK_PACK_LARGE
+#elif UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
+#define VALVE_CALLBACK_PACK_SMALL
+#else
+#error ???
+#endif
+
 using System;
 using System.Runtime.InteropServices;
 
 namespace Steamworks {
-	static class Packsize {
-#if UNITY_STANDALONE_WIN
+	public static class Packsize {
+#if VALVE_CALLBACK_PACK_LARGE
 		public const int value = 8;
-#elif UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
+#elif VALVE_CALLBACK_PACK_SMALL
 		public const int value = 4;
 #else
 #error ???
 #endif
 
-		public static bool SizeOf() {
-			int size = Marshal.SizeOf(typeof(ValvePackingSentinel_t));
-#if UNITY_STANDALONE_WIN
-			if (size == 32)
-				return true;
-#elif UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
-			if (size == 24)
-				return true;
+		public static bool Test() {
+			int sentinelSize = Marshal.SizeOf(typeof(ValvePackingSentinel_t));
+			int subscribedFilesSize = Marshal.SizeOf(typeof(RemoteStorageEnumerateUserSubscribedFilesResult_t));
+#if VALVE_CALLBACK_PACK_LARGE
+			if (sentinelSize != 32 || subscribedFilesSize != (1 + 1 + 1 + 50 + 100) * 4 + 4)
+				return false;
+#elif VALVE_CALLBACK_PACK_SMALL
+			if (sentinelSize != 24 || size != (1 + 1 + 1 + 50 + 100) * 4)
+				return false;
 #endif
-			return false;
+			return true;
 		}
-	}
 
-	[StructLayout(LayoutKind.Sequential, Pack = Packsize.value)]
-	struct ValvePackingSentinel_t {
-		uint m_u32;
-		ulong m_u64;
-		ushort m_u16;
-		double m_d;
-	};
+		[StructLayout(LayoutKind.Sequential, Pack = Packsize.value)]
+		struct ValvePackingSentinel_t {
+			uint m_u32;
+			ulong m_u64;
+			ushort m_u16;
+			double m_d;
+		};
+	}
 }
