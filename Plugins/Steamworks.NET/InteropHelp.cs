8cf93bb4166eea864f62f4d9ecd0cd9a6732a826
Riley Labrecque <rileylabrecque@gmail.com>
2014-07-20 02:33:40 -0600

    Match Making Key-Value Pair Marshaller

--- Plugins/Steamworks.NET/InteropHelp.cs
+++ Plugins/Steamworks.NET/InteropHelp.cs
@@ -171,4 +171,44 @@ namespace Steamworks {
 			}
 		}
 	}
+
+	// MatchMaking Key-Value Pair Marshaller
+	public class MMKVPMarshaller {
+		private IntPtr[] m_AllocatedMemory;
+		private IntPtr m_NativeArray;
+
+		public MMKVPMarshaller(MatchMakingKeyValuePair_t[] filters) {
+			if (filters == null) {
+				return;
+			}
+			
+			m_AllocatedMemory = new IntPtr[filters.Length];
+
+			int intPtrSize = Marshal.SizeOf(typeof(IntPtr));
+			m_NativeArray = Marshal.AllocHGlobal(intPtrSize * filters.Length);
+			for (int i = 0; i < filters.Length; i++) {
+				m_AllocatedMemory[i] = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(MatchMakingKeyValuePair_t)));
+				Marshal.StructureToPtr(filters[i], m_AllocatedMemory[i], false);
+
+				Marshal.WriteIntPtr(m_NativeArray, i * intPtrSize, m_AllocatedMemory[i]);
+			}
+		}
+
+		~MMKVPMarshaller() {
+			if (m_NativeArray != IntPtr.Zero) {
+				Marshal.FreeHGlobal(m_NativeArray);
+
+				foreach (IntPtr ptr in m_AllocatedMemory) {
+					if (ptr != IntPtr.Zero) {
+						Marshal.FreeHGlobal(ptr);
+					}
+				}
+			}
+
+		}
+
+		public static implicit operator IntPtr(MMKVPMarshaller that) {
+			return that.m_NativeArray;
+		}
+	}
 }
