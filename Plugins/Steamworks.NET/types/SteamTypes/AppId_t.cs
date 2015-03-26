db157d5857721226f95da306d59b86553c356ead
Riley Labrecque <rileylabrecque@gmail.com>
2015-03-26 13:44:03 -0600

    Update Copyright date to 2015

--- Plugins/Steamworks.NET/types/SteamTypes/AppId_t.cs
+++ Plugins/Steamworks.NET/types/SteamTypes/AppId_t.cs
@@ -1,5 +1,5 @@
-﻿// This file is provided under The MIT License as part of Steamworks.NET.
-// Copyright (c) 2013-2014 Riley Labrecque
+// This file is provided under The MIT License as part of Steamworks.NET.
+// Copyright (c) 2013-2015 Riley Labrecque
 // Please see the included LICENSE.txt for additional information.
 
 // Changes to this file will be reverted when you update Steamworks.NET
