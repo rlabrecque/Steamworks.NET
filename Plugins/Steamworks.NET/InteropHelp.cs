d22a4be2dcdea1e5581174e7b8bc2e583c5e0b0d
Riley Labrecque <rileylabrecque@gmail.com>
2015-06-28 15:57:31 -0600

    Temporarily disabled DllCheck.Test()

--- Plugins/Steamworks.NET/InteropHelp.cs
+++ Plugins/Steamworks.NET/InteropHelp.cs
@@ -170,32 +170,41 @@ namespace Steamworks {
 	}
 
 	public class DllCheck {
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		extern static int GetModuleFileName(IntPtr hModule, StringBuilder strFullPath, int nSize);
+
 		/// <summary>
 		/// This is an optional runtime check to ensure that the dlls are the correct version. Returns false only if the steam_api.dll is found and it's the wrong size or version number.
 		/// </summary>
 		public static bool Test() {
-			bool ret = true;
-
-#if STEAMWORKS_WIN || UNITY_EDITOR_WIN || (!UNITY_EDITOR && UNITY_STANDALONE_WIN)
-			ret = CheckSteamAPIDLL();
-#endif
-
-			return ret;
+			//bool ret = CheckSteamAPIDLL();
+			return true;
 		}
 
 		private static bool CheckSteamAPIDLL() {
-			string strCWD = System.IO.Directory.GetCurrentDirectory();
-
-			string file;
+#if STEAMWORKS_WIN || (UNITY_EDITOR_WIN && UNITY_STANDALONE) || (!UNITY_EDITOR && UNITY_STANDALONE_WIN)
+			string fileName;
 			int fileBytes;
 			if (IntPtr.Size == 4) {
-				file = System.IO.Path.Combine(strCWD, "steam_api.dll");
+				fileName = "steam_api.dll";
 				fileBytes = Version.SteamAPIDLLSize;
 			}
 			else {
-				file = System.IO.Path.Combine(strCWD, "steam_api64.dll");
+				fileName = "steam_api64.dll";
 				fileBytes = Version.SteamAPI64DLLSize;
 			}
+
+			IntPtr handle = GetModuleHandle(fileName);
+			if (handle == IntPtr.Zero) {
+				return true;
+			}
+
+			StringBuilder filePath = new StringBuilder(256);
+			GetModuleFileName(handle, filePath, filePath.Capacity);
+			string file = filePath.ToString();
 
 			// If we can not find the file we'll just skip it and let the DllNotFoundException take care of it.
 			if (System.IO.File.Exists(file)) {
@@ -208,7 +217,7 @@ namespace Steamworks {
 					return false;
 				}
 			}
-
+#endif
 			return true;
 		}
 	}
