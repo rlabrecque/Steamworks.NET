f92f6f1c7d8e88e9ae32a640c2c2fb30916677a2
Riley Labrecque <rileylabrecque@gmail.com>
2017-04-06 13:36:57 -0600

    Bump Version to 9.1.0

--- Plugins/Steamworks.NET/Steam.cs
+++ Plugins/Steamworks.NET/Steam.cs
@@ -8,7 +8,7 @@
 
 namespace Steamworks {
 	public static class Version {
-		public const string SteamworksNETVersion = "9.0.0";
+		public const string SteamworksNETVersion = "9.1.0";
 		public const string SteamworksSDKVersion = "1.39";
 		public const string SteamAPIDLLVersion = "03.75.32.07";
 		public const int SteamAPIDLLSize = 219424;
