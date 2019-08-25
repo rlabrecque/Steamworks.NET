aba015527bd70bbd41cd528ba608fa68dcacea0e
Riley Labrecque <git@rileylabrecque.com>
2019-08-25 15:28:44 -0700

    Update to Steamworks SDK 1.44 (The binaries are the same as 1.43 though??) (No ISteamNetworkingSockets support yet!)

--- Plugins/Steamworks.NET/Steam.cs
+++ Plugins/Steamworks.NET/Steam.cs
@@ -17,7 +17,7 @@ using IntPtr = System.IntPtr;
 namespace Steamworks {
 	public static class Version {
 		public const string SteamworksNETVersion = "13.0.0";
-		public const string SteamworksSDKVersion = "1.43";
+		public const string SteamworksSDKVersion = "1.44";
 		public const string SteamAPIDLLVersion = "04.95.20.30";
 		public const int SteamAPIDLLSize = 257312;
 		public const int SteamAPI64DLLSize = 288032;
