e64ec989b292552dbdc8c4dc25d81beebe5077ee
Riley Labrecque <git@rileylabrecque.com>
2021-03-21 15:40:12 -0700

    Update Version info for Release 15.0.0

--- Plugins/Steamworks.NET/Steam.cs
+++ Plugins/Steamworks.NET/Steam.cs
@@ -15,14 +15,6 @@ using System.Runtime.InteropServices;
 using IntPtr = System.IntPtr;
 
 namespace Steamworks {
-	public static class Version {
-		public const string SteamworksNETVersion = "15.0.0";
-		public const string SteamworksSDKVersion = "1.51";
-		public const string SteamAPIDLLVersion = "06.28.18.86";
-		public const int SteamAPIDLLSize = 239904;
-		public const int SteamAPI64DLLSize = 265504;
-	}
-
 	public static class SteamAPI {
 		//----------------------------------------------------------------------------------------------------------------------------------------------------------//
 		//	Steam API setup & shutdown
