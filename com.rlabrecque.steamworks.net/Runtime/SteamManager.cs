e48bc8f3fcc410d74ff2ffed62090178caf293a4
jclemente-statespace <jason@statespacelabs.com>
2021-09-23 17:00:31 -0500

    Fix for starting app outside of steam.

--- com.rlabrecque.steamworks.net/Runtime/SteamManager.cs
+++ com.rlabrecque.steamworks.net/Runtime/SteamManager.cs
@@ -116,7 +116,7 @@ public class SteamManager : MonoBehaviour {
 			// Once you get a Steam AppID assigned by Valve, you need to replace AppId_t.Invalid with it and
 			// remove steam_appid.txt from the game depot. eg: "(AppId_t)480" or "new AppId_t(480)".
 			// See the Valve documentation for more information: https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
-			if (SteamAPI.RestartAppIfNecessary(AppId_t.Invalid)) {
+			if (SteamAPI.RestartAppIfNecessary((AppId_t)714010)) {
 				Application.Quit();
 				return;
 			}
