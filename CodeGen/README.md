ef90d1679ea2b1e4d0107a5ebe7e74ce4ecf2f5f
Riley Labrecque <git@rileylabrecque.com>
2021-11-07 14:40:06 -0800

    CodeGen: Clean up the directory and README a bit

--- CodeGen/README.md
+++ CodeGen/README.md
@@ -1,4 +1,13 @@
-Steamworks.NET
-=======
+Steamworks.NET CodeGen
+======================
 
-The special sauce that generates the autogen/ folder of [Steamworks.NET](https://github.com/rlabrecque/Steamworks.NET)
+This is special sauce that generates the autogen/ and types/ directories.
+
+It uses [SteamworksParser](https://github.com/rlabrecque/SteamworksParser) to parse the Steamworks C++ header files, then converts them to C#.
+
+Usage
+-----
+
+1. If necessary update the files in `steam/`
+2. Open a command prompt to the CodeGen directory. (The script must be run from this directory or the relative paths will be broken.)
+3. Run `python3 Steamworks.NET_CodeGen.py`, preferably on a linux based OS to generate proper line endings. (I use WSL for this.)
--- CodeGen/Steamworks.NET_CodeGen.py
+++ CodeGen/Steamworks.NET_CodeGen.py
@@ -1,23 +1,16 @@
 import sys
 from SteamworksParser import steamworksparser
-import interfaces
-import constants
-import enums
-import structs
-import typedefs
+from src import interfaces
+from src import constants
+from src import enums
+from src import structs
+from src import typedefs
 
 def main():
-    if len(sys.argv) == 2:
-        path = sys.argv[1]
-    elif len(sys.argv) == 1:
-        path = "steam"
-    else:
-        print("Usage: Steamworks.NET_CodeGen.py [path/to/sdk/public/steam]")
-        print("       If a path is not included then a steam/ folder must exist within the cwd.")
-        return
+    steam_path = "steam/"
 
     steamworksparser.Settings.fake_gameserver_interfaces = True
-    ___parser = steamworksparser.parse(path)
+    ___parser = steamworksparser.parse(steam_path)
 
     interfaces.main(___parser)
     constants.main(___parser)
