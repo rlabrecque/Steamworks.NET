a2fc889ab2672981ec3e6225d551d86ce6923121
Riley Labrecque <git@rileylabrecque.com>
2024-08-17 09:10:37 -0700

    Bump Version to 2024.8.0

--- com.rlabrecque.steamworks.net/Runtime/Version.cs
+++ com.rlabrecque.steamworks.net/Runtime/Version.cs
@@ -13,7 +13,7 @@
 
 namespace Steamworks {
 	public static class Version {
-		public const string SteamworksNETVersion = "20.2.0";
+		public const string SteamworksNETVersion = "2024.8.0";
 		public const string SteamworksSDKVersion = "1.60";
 		public const string SteamAPIDLLVersion = "08.97.99.70";
 		public const int SteamAPIDLLSize = 265064;
