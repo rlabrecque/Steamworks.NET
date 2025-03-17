b9d01ecc88c2b88789f01952434a2b608fa65a27
MindsEyeGames <mindseyegames@gmail.com>
2025-03-17 06:53:43 -0400

    Update Steamworks API to 1.6.2
    Attempted to follow the instructions in CodeGen/README.md, and then updated the dll/so/dylib files and the info in Version.cs
    
    This was done on Windows (and thus there are unfortunately line ending changes in this), so I assume it's preferable to just have someone more connected to the project do it directly on Linux :/ But figured I would try to contribute a version update since I was asking for it in https://github.com/rlabrecque/Steamworks.NET/issues/687
    
    Additionally, some parts of the API seem  to have been deprecated/removed - those functions look less common but worth calling out.

--- CodeGen/steam/isteamuserstats.h
+++ CodeGen/steam/isteamuserstats.h
@@ -88,9 +88,11 @@ struct LeaderboardEntry_t
 class ISteamUserStats
 {
 public:
-	// Ask the server to send down this user's data and achievements for this game
-	STEAM_CALL_BACK( UserStatsReceived_t )
-	virtual bool RequestCurrentStats() = 0;
+
+	// Note: this call is no longer required as it is managed by the Steam client
+	// The game stats and achievements will be synchronized with Steam before
+	// the game process begins.
+	// virtual bool RequestCurrentStats() = 0;
 
 	// Data accessors
 	STEAM_FLAT_NAME( GetStatInt32 )
@@ -303,7 +305,7 @@ public:
 
 };
 
-#define STEAMUSERSTATS_INTERFACE_VERSION "STEAMUSERSTATS_INTERFACE_VERSION012"
+#define STEAMUSERSTATS_INTERFACE_VERSION "STEAMUSERSTATS_INTERFACE_VERSION013"
 
 // Global interface accessor
 inline ISteamUserStats *SteamUserStats();
@@ -454,19 +456,6 @@ struct LeaderboardUGCSet_t
 	enum { k_iCallback = k_iSteamUserStatsCallbacks + 11 };
 	EResult m_eResult;				// The result of the operation
 	SteamLeaderboard_t m_hSteamLeaderboard;	// the leaderboard handle that was
-};
-
-
-//-----------------------------------------------------------------------------
-// Purpose: callback indicating that PS3 trophies have been installed
-//-----------------------------------------------------------------------------
-struct PS3TrophiesInstalled_t
-{
-	enum { k_iCallback = k_iSteamUserStatsCallbacks + 12 };
-	uint64	m_nGameID;				// Game these stats are for
-	EResult m_eResult;				// The result of the operation
-	uint64 m_ulRequiredDiskSpace;	// If m_eResult is k_EResultDiskFull, will contain the amount of space needed to install trophies
-
 };
 
 
