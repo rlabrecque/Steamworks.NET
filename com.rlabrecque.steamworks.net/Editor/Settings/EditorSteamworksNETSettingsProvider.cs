8761b716d764729c3a91d1d3979da3472ac4f63b
Marllon Vilano <sandolkakos@hotmail.com>
2025-06-06 02:01:02 -0300

    Add a settings menu option to disable Steamworks.NET from automatically adding define symbols to the project

---
+++ com.rlabrecque.steamworks.net/Editor/Settings/EditorSteamworksNETSettingsProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+public sealed class EditorSteamworksNETSettingsProvider : SettingsProvider
+{
+    private const string SettingsPath = "Project/Steamworks.NET";
+
+    private EditorSteamworksNETSettingsProvider(SettingsScope scopes, IEnumerable<string> keywords = null)
+        : base(SettingsPath, scopes, keywords)
+    {
+    }
+
+    public override void OnActivate(string searchContext, VisualElement rootElement)
+    {
+        base.OnActivate(searchContext, rootElement);
+        rootElement.Add(new EditorSteamworksNETSettingsElement());
+    }
+
+    /// <summary>
+    /// Method which adds your settings provider to ProjectSettings
+    /// </summary>
+    /// <returns>A <see cref="EditorSteamworksNETSettingsProvider"/>.</returns>
+    [SettingsProvider]
+    public static SettingsProvider CreateSettingsProvider()
+    {
+        return new EditorSteamworksNETSettingsProvider(SettingsScope.Project);
+    }
+}
